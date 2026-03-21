2b470af0ab305b4526dbd30508cede2b48e71413
EthanBrooks-sheng <1156270547@qq.com>
2026-03-21 13:20:36 +0800

    complete StarTriangle5.java

--- hw02/src/StarTriangle5.java
+++ hw02/src/StarTriangle5.java
@@ -5,8 +5,15 @@ public class StarTriangle5 {
      */
    public static void starTriangle5() {
       // TODO: Fill in this function
+       for (int i = 1; i <= 5; i++) {
+           String stars = "*".repeat(i);
+           String spaces = " ".repeat(5 - i);
+           System.out.print(spaces + stars);
+           if (i != 5)
+               System.out.println();
+       }
    }
-   
+
    public static void main(String[] args) {
       starTriangle5();
    }
