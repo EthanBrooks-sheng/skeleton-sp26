3603496a334df4461ab99b21152ab82dfcfc8423
EthanBrooks-sheng <1156270547@qq.com>
2026-03-21 13:18:08 +0800

    complete DoubleUp.java

--- hw02/src/DoubleUp.java
+++ hw02/src/DoubleUp.java
@@ -1,3 +1,5 @@
+import org.junit.platform.commons.util.ToStringBuilder;
+
 public class DoubleUp {
    /**
      * Returns a new string where each character of the given string is repeated twice.
@@ -5,6 +7,13 @@ public class DoubleUp {
      */
    public static String doubleUp(String s) {
       // TODO: Fill in this function
+      if (!s.isEmpty()){
+          String doubleup = "";
+          for (int i = 0; i < s.length(); i++) {
+              doubleup = doubleup + s.charAt(i) + s.charAt(i);
+          }
+          return doubleup;
+      }
       return null;
    }
    
