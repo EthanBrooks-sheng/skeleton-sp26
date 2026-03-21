58068999eed21ae10a962163be8e701729f56e44
EthanBrooks-sheng <1156270547@qq.com>
2026-03-21 13:21:06 +0800

    complete StarTriangleN.java

--- hw02/src/StarTriangleN.java
+++ hw02/src/StarTriangleN.java
@@ -5,6 +5,16 @@ public class StarTriangleN {
      */
    public static void starTriangle(int N) {
       // TODO: Fill in this function
+       for (int i = 1; i <= N; i++) {
+           for (int j = 1; j <= N - i; j++) {
+               System.out.print(" ");
+           }
+           for (int k = 1; k <= i; k++) {
+               System.out.print("*");
+           }
+           if (i != N)
+               System.out.println();
+       }
    }
    
    public static void main(String[] args) {
