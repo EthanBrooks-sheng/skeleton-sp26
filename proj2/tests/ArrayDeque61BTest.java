de42f5722828ea10bcfea194c10c57b6041fd87e
EthanBrooks-sheng <1156270547@qq.com>
2026-03-28 20:00:49 +0800

    completed the test of addFirst and addLast

--- proj2/tests/ArrayDeque61BTest.java
+++ proj2/tests/ArrayDeque61BTest.java
@@ -3,4 +3,21 @@ import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
 
 public class ArrayDeque61BTest {
+    @Test
+    public void addFirstAndAddLastTest() {
+        Deque61B<Integer> expected = new ArrayDeque61B<>();
+        assertThat(expected.toList()).isEmpty();
+
+        expected.addFirst(1);  // [1]
+        assertThat(expected.toList()).containsExactly(1).inOrder();
+        expected.addLast(2);   // [1, 2]
+        assertThat(expected.toList()).containsExactly(1, 2).inOrder();
+        expected.addFirst(0);  // [0, 1, 2]
+        expected.addLast(3);   // [0, 1, 2, 3]
+        expected.addFirst(10); // [10, 0, 1, 2, 3]
+        expected.addLast(15);  // [10, 0, 1, 2, 3, 15]
+        expected.addLast(20);  // [10, 0, 1, 2, 3, 15, 20]
+        expected.addLast(25);  // [10, 0, 1, 2, 3, 15, 20, 25]
+        assertThat(expected.toList()).containsExactly(10, 0, 1, 2, 3, 15, 20, 25).inOrder();
+    }
 }
